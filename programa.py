501d6f6b3c6a736e12517b48948bbc675991a696
danilo de lima <danilolima28608@gmail.com>
2025-05-16 17:49:41 -0300

    Adicionado pesquisa de satisfação

--- programa.py
+++ programa.py
@@ -1,4 +1,4 @@
 import requests
 print("meu programa é relevante para aula")
-nota = input("Numa escala de 0 a 1235, como você avalia  esse projeto ")
-print(f"legal{nota}")
+nota = input("Numa escala de 0 a 1235, como você avalia  a relevância desse projeto ")
+print(f"esse projeto tem uma relevância {nota}")
